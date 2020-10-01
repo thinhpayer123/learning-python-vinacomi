f05bf600ded6746e74ffb702e42d7c758f3e58ab
cuongnd <cuongnd@upstart.vn>
2020-10-01 23:56:25 +0700

    Initial commit

---
+++ application/config.py
@@ -0,0 +1,14 @@
+class Config(object):
+    DEBUG = True
+    STATIC_URL = "static"
+    SQLALCHEMY_DATABASE_URI = 'postgresql://genqr_user:123456abcA@localhost:5432/genqr'
+    AUTH_LOGIN_ENDPOINT = 'login'
+    AUTH_PASSWORD_HASH = 'sha512_crypt'
+    AUTH_PASSWORD_SALT = 'ruewhndjsa17heaw'
+    SECRET_KEY = 'e2q8dhaushdauwd7qye'
+    SESSION_COOKIE_SALT = 'dhuasud819wubadhysagd'
+    FS_ROOT = "static/upload/"
+    IMAGE_SERVICE_URL = "http://0.0.0.0:8090/static/upload/"
+    FILE_SERVICE_URL = "http://0.0.0.0:8090/static/upload/"
+    QR_SERVICE_URL = "http://0.0.0.0:8090/"
+    QR_ARCHIVE = "static/upload/qrcode/"
---
+++ application/controllers/__init__.py
@@ -0,0 +1,22 @@
+# Register Blueprints/Views.
+from gatco.response import text, json
+from application.extensions import jinja
+
+def init_views(app):
+    import application.controllers.user
+    import application.controllers.quocgia
+    import application.controllers.tinhthanh
+
+    import application.controllers.khachhang
+    import application.controllers.hanghoa
+    import application.controllers.hoadon
+    import application.controllers.sinhvien
+    
+    
+    @app.route('/')
+    def index(request):
+        #return text("Index")
+        return jinja.render('index.html', request)
+
+    
+    
