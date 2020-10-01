1e48fad0f2edd1c0ef19b0968f9dd5fed4502a32
cuongnd <cuongnd@upstart.vn>
2020-10-02 05:20:07 +0700

    update api

---
+++ .history/application/controllers/sinhvien_20201002031654.py
@@ -0,0 +1,94 @@
+from application.extensions import apimanager
+from application.models.model import Student, QRUser, QRworker
+from application.extensions import auth
+from application.database import db
+from gatco.exceptions import ServerError
+from sqlalchemy import create_engine
+import os
+import random
+import string
+import aiofiles
+import time
+from gatco.response import json
+from application.server import app
+from application.config import Config
+import psycopg2
+config = Config()
+import pandas as pd
+import xlrd
+import qrcode
+import shutil
+import asyncio
+
+
+def auth_func(request=None, **kw):
+    #uid = auth.current_user(request)
+    #if uid is None:
+    #    raise ServerError("abc")
+    pass
+
+
+@app.route('/api/v1/file/upload', methods=['GET', 'POST'])
+async def file_load(request):
+    path = request.args.get("path", None)
+    ret = None
+    # url_qr = config.QR_SERVICE_URL
+    # url = config.FILE_SERVICE_URL
+    fsroot = config.FS_ROOT
+
+    if request.method == 'POST':
+        file = request.files.get('file', None)
+        if file:
+
+            extname = request.files.get('file').name
+            if not os.path.exists(fsroot):
+                os.makedirs(fsroot)
+            subPath = ""
+            if path is not None:
+                subPath = path + "/"
+                if not os.path.exists(fsroot + subPath):
+                    os.makedirs(fsroot + subPath)
+            async with aiofiles.open(fsroot + subPath + extname, 'wb+') as f:
+                await f.write(file.body)
+            link_local = fsroot + subPath + extname
+            print(link_local)
+            data = pd.read_excel(link_local)
+            print(data)
+            df = pd.DataFrame(data, columns=['student_school_year', 'student_class', 'student_id', 'student_name', 'birthday', 'gender'])
+            print(df)
+            alchemyEngine = create_engine('postgresql://icangteen_user:123456abcA@localhost:5432/icangteen', pool_recycle=3600);
+            postgreSQLConnection = alchemyEngine.connect();
+            postgreSQLTable = 'student';
+            df.to_sql(postgreSQLTable, alchemyEngine, if_exists='append', index=False)
+            # id  = request.args.get("id")
+            # type(link_local)
+            # print(id)
+            # print(            type(link_local))
+            # qrworker = QRworker()
+            # qrworker.uid = id
+            # qrworker.saveDirectory = link_local
+            # qrworker.status = 'PENDING'
+            # qrworker.namefile = extname
+            # linkdowload = qrworker.linkdowload
+            # db.session.add(qrworker)
+            # db.session.commit()
+            job = db.session.query(Student).filter(Student.id == 12).with_for_update().first()
+            print(job)
+#
+
+
+            ret = {
+                "notify":"upload file success ",
+                # "id": id
+
+            }
+    return json(ret)
+
+
+apimanager.create_api(collection_name='qrworker', model=QRworker,
+    methods=['GET', 'POST', 'DELETE', 'PUT'],
+    url_prefix='/api/v1',
+    preprocess=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func], POST=[auth_func], PUT_SINGLE=[auth_func]),
+    )
+
+
---
+++ .history/application/models/model_20201002021613.py
@@ -0,0 +1,159 @@
+""" Module represents a User. """
+
+from sqlalchemy import (
+    Column, String, Integer,
+    DateTime, Date, Boolean,
+    ForeignKey
+)
+
+from sqlalchemy import (
+    Column, String, Integer, DateTime, Date, Boolean, DECIMAL, ForeignKey, Text
+)
+from sqlalchemy.dialects.postgresql import UUID
+
+from sqlalchemy.orm import relationship, backref
+
+from application.database import db
+from application.database.model import CommonModel, default_uuid
+
+
+roles_users = db.Table('roles_users',
+                       db.Column('user_id', Integer, db.ForeignKey('users.id', ondelete='cascade'), primary_key=True),
+                       db.Column('role_id', Integer, db.ForeignKey('role.id', onupdate='cascade'), primary_key=True))
+
+
+class Role(CommonModel):
+    __tablename__ = 'role'
+    id = db.Column(Integer, autoincrement=True, primary_key=True)
+    role_name = db.Column(String(100), index=True, nullable=False, unique=True)
+    display_name = db.Column(String(255), nullable=False)
+    description = db.Column(String(255))
+
+
+
+
+
+
+class User(CommonModel):
+    __tablename__ = 'users'
+
+    id = db.Column(Integer, autoincrement=True, primary_key=True)
+
+    # Authentication Attributes.
+    user_name = db.Column(String(255), nullable=False, index=True)
+    full_name = db.Column(String(255), nullable=True)
+    email = db.Column(String(255), nullable=False, index=True)
+    password = db.Column(String(255), nullable=False)
+    salt = db.Column(String(255), nullable=False)
+
+    # Permission Based Attributes.
+    is_active = db.Column(Boolean, default=True)
+
+    # Methods
+    def __repr__(self):
+        """ Show user object info. """
+        return '<User: {}>'.format(self.id)
+
+
+
+# class sinhVien(CommonModel):
+#     __tablename__ = 'sinhvien'
+#     id = db.Column(Integer, primary_key=True)
+#     masv = db.Column(String(255), nullable=False)
+#     tensv = db.Column(String(255), nullable=False)
+#     malop = db.Column(String(255))
+#     birthday = db.Column(String(255))
+#     gioitinh = db.Column(String(10))
+
+
+class Student(CommonModel):
+    __tablename__ = 'student'
+    _id = db.Column(Integer, primary_key=True)
+    student_school_year = db.Column(String(30), nullable=False)
+    student_class = db.Column(String(30), nullable=False)
+    student_id = db.Column(String(20), nullable=False)
+    student_name = db.Column(String(255), nullable=False)
+    birthday = db.Column(String(20))
+    gender = db.Column(String(10))
+
+
+class QRUser(CommonModel):
+    __tablename__ = 'qruser'
+    _id = db.Column(Integer, primary_key=True)
+    nameqr = db.Column(String(255), nullable=False)
+    saveDirectory = db.Column(String(255), nullable=False)
+
+
+class QRworker(CommonModel):
+    __tablename__ = 'qrworker'
+    _id = db.Column(Integer, primary_key=True)
+    uid = db.Column(String(30), nullable=False)
+    save_directory = db.Column(String(255), nullable=False)
+    status = db.Column(String(10))
+    namefile = db.Column(String(255))
+    dowload_url = db.Column(String(255))
+
+# class QuocGia(CommonModel):
+#     __tablename__ = 'quocgia'
+#     id = db.Column(Integer, primary_key=True)
+#     ma = db.Column(String(255), unique=True)
+#     ten = db.Column(String(255), nullable=False)
+#     mota = db.Column(String(255), nullable=True)
+#     tinhthanh = db.relationship("TinhThanh", order_by="TinhThanh.id", cascade="all, delete-orphan")
+#
+# class TinhThanh(CommonModel):
+#     __tablename__ = 'tinhthanh'
+#     id = db.Column(Integer, primary_key=True)
+#     ma = db.Column(String(255), unique=True)
+#     ten = db.Column(String(255), nullable=False)
+#     quocgia_id = db.Column(Integer, ForeignKey('quocgia.id'), nullable=False)
+#     quocgia = db.relationship('QuocGia')
+#
+# class KhachHang(CommonModel):
+#     __tablename__ = 'khachhang'
+#     id = db.Column(Integer, primary_key=True)
+#     ma = db.Column(String(255), unique=True)
+#     ten = db.Column(String(255), nullable=False)
+#     quocgia_id = db.Column(Integer, ForeignKey('quocgia.id'), nullable=False)
+#     quocgia = db.relationship('QuocGia')
+#     sodienthoai = db.Column(String(255))
+#     email = db.Column(String(255))
+#     diachi = db.Column(String(255))
+#
+# class HangHoa(CommonModel):
+#     __tablename__ = 'hanghoa'
+#     id = db.Column(Integer, primary_key=True)
+#     ma = db.Column(String(255), unique=True)
+#     ten = db.Column(String(255), nullable=False)
+#     gia = db.Column(Integer)
+#     ghichu = db.Column(String(255))
+#
+# class HoaDon(CommonModel):
+#     __tablename__ = 'hoadon'
+#     id = db.Column(Integer, primary_key=True)
+#     ma = db.Column(String(255), unique=True)
+#     ghichu = db.Column(String(255))
+#     khachhang_id = db.Column(Integer, ForeignKey('khachhang.id'), nullable=False)
+#     tenkhachhang = db.Column(String(255))
+#     ngaymua = db.Column(DateTime)
+#
+#     thanhtien = db.Column(DECIMAL)
+#     vat = db.Column(Integer, default=10)
+#     tongtien = db.Column(DECIMAL)
+#
+#     chitiethoadon = db.relationship("ChiTietHoaDon", order_by="ChiTietHoaDon.id", cascade="all, delete-orphan", lazy='dynamic')
+#
+# class ChiTietHoaDon(CommonModel):
+#     __tablename__ = 'chitiethoadon'
+#     id = db.Column(Integer, primary_key=True)
+#     hoadon_id = db.Column(Integer, ForeignKey('hoadon.id'), nullable=False)
+#
+#     hanghoa_id = db.Column(Integer, ForeignKey('hanghoa.id'), nullable=False)
+#     mahanghoa = db.Column(String(255))
+#     tenhanghoa = db.Column(String(255))
+#
+#     soluong = db.Column(Integer)
+#     dongia = db.Column(Integer)
+#     thanhtien = db.Column(Integer)
+
+
