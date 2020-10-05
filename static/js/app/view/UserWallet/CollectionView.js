d750fe318a7463743af507ab5261b1f275edbd4f
xuanhoa <xuanhoa19980317@gmail.com>
2020-10-06 01:32:52 +0700

    tesst

--- static/js/app/view/UserWallet/CollectionView.js
+++ static/js/app/view/UserWallet/CollectionView.js
@@ -69,21 +69,6 @@ define(function (require) {
 		],
     	uiControl:{
     		fields: [
-<<<<<<< HEAD
-	    	    //  { 
-	    	    // 	field: "id",label:"ID",
-	    	    //  },
-	    	     { field: "company_id", label: "ID công ty"},
-				  { field: "company_type", label: "kiểu công ty", width:250 },
-				  { field: "user_id", label: "ID người dùng" },
-				  { field: "wallet_id", label: "ID ví tiền" },
-				  { field: "relationship", label: "Quan hệ" },
-				  { field: "company_no", label: "Hóa đơn công ty" },
-				  { field: "user_no", label: "Hóa đơn người dùng" },
-				//   { field: "extra_data", label: "extra_data" },
-
-
-=======
 	    	     { 
 	    	    	field: "id",label:"ID",
 				 },
@@ -112,7 +97,6 @@ define(function (require) {
 				 { 
 	    	    	field: "extra_data",label:"Thông Tin Chi Tiết",
 	    	     },
->>>>>>> 07f6e8832b42d3ca8513c373df1a80e44ac99421
 
 		     ],
 		     onRowClick: function(event){
