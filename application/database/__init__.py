6c4035d455b8699caa8dc3d0e0dbdd861a052194
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-10-20 02:16:36 +0700

    update transaction

--- application/database/__init__.py
+++ application/database/__init__.py
@@ -1,5 +1,10 @@
 from gatco_sqlalchemy import SQLAlchemy
+import redis
+
 db = SQLAlchemy()
+
+
+redisdb = redis.StrictRedis(host='0.0.0.0', port=6379)
 
 def init_database(app):
     db.init_app(app)
