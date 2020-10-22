60c839bc1637b436671dd38c8172cf25472f1d00
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-10-22 19:59:27 +0700

    update mobile api

--- application/controllers/app_mobile_api.py
+++ application/controllers/app_mobile_api.py
@@ -4,7 +4,7 @@ from application.extensions import auth
 from gatco.exceptions import ServerError
 from application.server import app
 from gatco.response import json
-from application.database import db
+from application.database import db,redisdb
 from gatco.exceptions import ServerError
 from sqlalchemy import create_engine
 import os
@@ -25,226 +25,287 @@ import ujson
 import hashlib
 import requests
 import aiohttp
+# import async
+
+
+@app.route("/api/v1/add_membercard",methods = ['POST'])
+async def add_membercard(request):
+    if request.method == 'POST':
+        params = request.json
+        membercard_id_owner = params.get("membercard_id")
+        company_id = params.get("company_id")        
+        if company_id is not None:
+            company = db.session.query(Company).filter(Company.company_id == company_id).first()
+            name_company = company.name
+            point_name = company.point_name if company is not None else None
+
+        if membercard_id_owner is not None:
+            getWallet_id = db.session.query(MemberCard).filter(MemberCard.membercard_id == membercard_id_owner).first()
+            wallet_id = getWallet_id.wallet_id
+            wallet_name  = getWallet_id.user_name
+            url = app.config.get("HEOVANG_WALLET_API_URL") + "/wallet/api/v1/get_point_balance_by_uid"
+            x_wallet_user_token = ''.join(random.choice(string.ascii_letters) for i in range(16))
+            headers = {
+                "Content-Type": "application/ecmascript",
+                "X-WALLET-USER-TOKEN": x_wallet_user_token
+
+            }
+            data  = {
+                "point_name": point_name,
+                "wallet_id": wallet_id
+            }
+            async with aiohttp.ClientSession(headers=headers, json_serialize=ujson.dumps) as session:
+                async with session.post(url, json=data) as response:
+                    print(response.status, await response.text())
+                    if response.status == 200:
+                        resp = await response.json()
+                        #                         {
+                        #     "main": 500725000,
+                        #     "sub": 0,
+                        #     "value": 500725000,
+                        #     "main_value": 500725000,
+                        #     "sub_value": 0
+                        # }
+                        value =  resp.get("main")
+                        print(value)
+
+        return json({
+                    "name_company": name_company,
+                    "wallet_id": wallet_id,
+                    "wallet_name": wallet_name,
+                    "point": value
+                })
+            # truyen wallet 
+        
+        
+@app.route("/api/v1/add_subcribe", methods =['POST'])
+def add_subcribe(request):
+    if request.method == 'POST':
+        params = request.json
+        token = request.args.get('token', None)
+        wallet_id = params.get("wallet_id")
+        uid = redisdb.get("sessions:"+token)
+        # user_subcribe = db.session.query(User).filter(User.id == uid).first()
+        wallet  = db.session.query(WalletUser).filter(WalletUser.wallet_id == wallet_id).first()
+        if wallet is not None: 
+            wallet.relationship = uid
+            db.session.commit()
+            return json({
+                "message":"Đã thêm ví thành công vào tài khoản"
+            })
+        else:
+            return({"ERROR_MASSAGE":"Thêm ví thất bại"})
 
 
 
 
-@app.route('/api/v1/get_item_by_day', methods=['GET'])
-async def get_all_item_by_day(request):
-    if request.method == 'GET':
-        brand_id = "BRAND-YHXD"
-        trandate = int(datetime.datetime.strptime(time.strftime('%m/%d/%Y'), '%m/%d/%Y').strftime("%s"))
-        print(brand_id,trandate)
-        url = app.config.get("GET_SALE_MANAGER") + "/api/v1/partners/get-sales"
-        private_key = app.config.get("ACCESS_PRIVATE_KEY_SALE_MANAGER_ITEM")
-        access_token = app.config.get("ACCESS_TOKEN_SALE_MANAGER_ITEM")
-        key = access_token + private_key 
-        print(key)
-        secret_key  = hashlib.md5(key.encode())
-        secret_key_sent=  secret_key.hexdigest()
-        print(secret_key_sent)
+
+
+# @app.route('/api/v1/get_item_by_day', methods=['GET'])
+# async def get_all_item_by_day(request):
+#     if request.method == 'GET':
+#         brand_id = "BRAND-YHXD"
+#         trandate = int(datetime.datetime.strptime(time.strftime('%m/%d/%Y'), '%m/%d/%Y').strftime("%s"))
+#         print(brand_id,trandate)
+#         url = app.config.get("GET_SALE_MANAGER") + "/api/v1/partners/get-sales"
+#         private_key = app.config.get("ACCESS_PRIVATE_KEY_SALE_MANAGER_ITEM")
+#         access_token = app.config.get("ACCESS_TOKEN_SALE_MANAGER_ITEM")
+#         key = access_token + private_key 
+#         secret_key  = hashlib.md5(key.encode())
+#         secret_key_sent=  secret_key.hexdigest()
+
+
+#         headers = {
+#             "access-token": access_token,
+#             "secret-key": secret_key_sent
+#         }
+#         param = {
+#             # "sale_id": sale_id,
+#             "brand-id": brand_id ,
+#             "tran-date": trandate
+#         }
+#         async with aiohttp.ClientSession(headers=headers, json_serialize=ujson.dumps) as session:
+#             async with session.get(url, params=param) as response:
+#                 print(response.status, await response.text())
+#                 if response.status == 200:
+#                     resp = response.json()
+#                     print(resp)
+#             return  json({"notify":"success"})
 
 
 
-        headers = {
-            "access-token": access_token,
-            "secret-key": secret_key_sent
-        }
-        param = {
-            "brand-id": brand_id ,
-            "tran-date": trandate
-        }
-        async with aiohttp.ClientSession(headers=headers, json_serialize=ujson.dumps) as session:
-            async with session.get(url, params=param) as response:
-                print(response.status, await response.text())
-                if response.status == 200:
-                    resp = response.json()
-                    print(resp)
-            return  json({"notify":"success"})
+    # {
+    #   "id": "8a935478-0c2b-41fb-97a2-a0aae2453e6a",
+    #   "created_at": 1603327764,
+    #   "updated_at": 1603327764,
+    #   "sale_id": "6ZBN9264QMVEHMVIK07K9N", // tran_id trong bang transaction 
+    #   "origin_sale_id": "",
+    #   "tran_date": 1603299600,
+    #   "sale": {
+    #     "vat": 0,
+    #     "note": "",
+    #     "status": 1,
+    #     "area_id": null,
+    #     "is_temp": null,
+    #     "room_id": "",
+    #     "sale_id": "6ZBN9264QMVEHMVIK07K9N",
+    #     "tran_id": "TA",
+    #     "tran_no": "S2400004",
+    #     "user_id": "SO 2 (S.O)",
+    #     "shift_id": "SOO1941919841HMVIL5H6BF",
+    #     "store_id": "PVV6ABM1ZEKQ",
+    #     "vat_sign": null,
+    #     "date_last": 1603299600,
+    #     "hour_last": 7,
+    #     "sale_sign": null,
+    #     "source_id": "10000134",
+    #     "tran_date": 1603299600,
+    #     "get_amount": 0,
+    #     "session_id": 0,
+    #     "store_name": "MAYA SCHOOL - THẠCH THẤT",
+    #     "customer_id": null,
+    #     "device_code": "6ZBN9264QMVE",
+    #     "minute_last": 49,
+    #     "number_male": null,
+    #     "print_count": 0,
+    #     "vat_content": null,
+    #     "vat_tran_no": null,
+    #     "amount_point": 0,
+    #     "coupon_count": null,
+    #     "last_version": "1.1.12",
+    #     "shift_charge": 0,
+    #     "source_fb_id": "0",
+    #     "station_code": null,
+    #     "tran_no_temp": "S2400004",
+    #     "vat_tax_code": null,
+    #     "coupon_amount": 0,
+    #     "exchange_rate": 1,
+    #     "membership_id": null,
+    #     "number_female": null,
+    #     "number_people": 1,
+    #     "return_amount": 0,
+    #     "store_address": null,
+    #     "card_info_code": null,
+    #     "deposit_amount": 0,
+    #     "discount_extra": 0,
+    #     "origin_sale_id": "",
+    #     "payment_status": null,
+    #     "service_charge": 0,
+    #     "store_latitude": null,
+    #     "tran_date_orig": 1603299600,
+    #     "dinner_table_id": null,
+    #     "pr_key_bookings": 0,
+    #     "store_longitude": null,
+    #     "address_delivery": null,
+    #     "currency_type_id": "VND",
+    #     "vat_company_name": null,
+    #     "membership_id_new": null,
+    #     "source_voucher_id": null,
+    #     "vat_customer_name": null,
+    #     "membership_type_id": "",
+    #     "membership_voucher": 0,
+    #     "vat_payment_method": null,
+    #     "membership_birthday": "",
+    #     "vat_customer_address": null,
+    #     "amount_discount_extra2": 0
+    #   },
+    #   "sale_detail": [
+    #     {
+    #       "fix": 0,
+    #       "tax": 0,
+    #       "note": "",
+    #       "is_fc": 0,
+    #       "amount": 25000,
+    #       "is_kit": 1,
+    #       "is_set": 0,
+    #       "number": 1,
+    #       "ots_ta": 0,
+    #       "is_gift": 0,
+    #       "item_id": "CHAO2",
+    #       "payment": 0,
+    #       "printed": 0,
+    #       "sale_id": "6ZBN9264QMVEHMVIK07K9N",
+    #       "tran_id": "TA",
+    #       "unit_id": "SUAT",
+    #       "user_id": "SO 2 (S.O)",
+    #       "discount": 0,
+    #       "end_date": 1603299600,
+    #       "hour_end": 7,
+    #       "quantity": 1,
+    #       "shift_id": "SOO1941919841HMVIL5H6BF",
+    #       "price_org": 25000,
+    #       "sale_date": 1603299600,
+    #       "cost_price": 0,
+    #       "hour_start": 7,
+    #       "is_invoice": 1,
+    #       "is_service": 0,
+    #       "list_order": 0,
+    #       "minute_end": 49,
+    #       "package_id": "",
+    #       "price_sale": 25000,
+    #       "description": "Cháo sườn",
+    #       "is_eat_with": 0,
+    #       "amount_point": 0,
+    #       "minute_start": 49,
+    #       "payment_type": "HEOVANGSCAN",
+    #       "pr_key_order": 0,
+    #       "promotion_id": null,
+    #       "source_fb_id": "0",
+    #       "stop_service": 0,
+    #       "printed_label": 0,
+    #       "is_print_label": 0,
+    #       "parent_item_id": "",
+    #       "promotion_name": null,
+    #       "sale_detail_id": null,
+    #       "temp_calculate": 0,
+    #       "item_id_mapping": "",
+    #       "quantity_at_temp": 0,
+    #       "distribute_discount_extra2": 0
+    #     }
+    #   ],
+    #   "sale_payment_method": [
+    #     {
+    #       "amount": 25000,
+    #       "sale_id": "6ZBN9264QMVEHMVIK07K9N",
+    #       "user_id": "SO 2 (S.O)",
+    #       "shift_id": "SOO1941919841HMVIL5H6BF",
+    #       "trace_no": "6ZBN9264QMVEHMVIK07K9N",
+    #       "tran_date": 1603299600,
+    #       "tran_hour": 7,
+    #       "list_order": 0,
+    #       "amount_orig": 25000,
+    #       "tran_minute": 49,
+    #       "source_fb_id": "0",
+    #       "exchange_rate": 1,
+    #       "amount_orig_get": 0,
+    #       "currency_type_id": "VND",
+    #       "payment_method_id": "HEOVANGSCAN",
+    #       "amount_orig_return": 0,
+    #       "exchange_rate_return": 1,
+    #       "sale_payment_method_id": null,
+    #       "currency_type_id_return": "VND"
+    #     }
+    #   ],
+    #   "is_web": false,
+    #   "is_group_bill": false,
+    #   "source_fb_id": "",
+    #   "check_sum": "",
+    #   "amount_org": 25000,
+    #   "amount": 25000,
+    #   "amount_discount_price": 0,
+    #   "amount_discount_detail": 0,
+    #   "amount_discount_extra": 0,
+    #   "amount_service_charge": 0,
+    #   "amount_vat": 0,
+    #   "coupon_amount": 0,
+    #   "coupon_amount_paid": 0,
+    #   "ship_charge": 0,
+    #   "pos_type": "POS_FABI",
+    #   "store_id": "PVV6ABM1ZEKQ",
+    #   "brand_id": "BRAND-YHXD",
+    #   "company_id": "KORJBAZGV629"
+    # },
+
+
     
 
-
-    
-# https://apidwtest.ipos.vn/api/v1/partners/get-sales?sale-id=RMYZ8ZKV8YMZ4VR6KH68J2P&brand-id=BRAND-8FDR&tran-date=1603213200
-
-# {
-#   "data": {
-#     "id": "de07a794-1fb3-4116-8957-8ad3127b0bd9",
-#     "created_at": 1603250759,
-#     "updated_at": 1603250759,
-#     "sale_id": "RMYZ8ZKV8YMZ4VR6KH68J2P",
-#     "origin_sale_id": "",
-#     "tran_date": 1603213200,
-#     "sale": {
-#       "vat": 0,
-#       "note": "",
-#       "status": 1,
-#       "area_id": null,
-#       "is_temp": null,
-#       "room_id": "",
-#       "sale_id": "RMYZ8ZKV8YMZ4VR6KH68J2P",
-#       "tran_id": "TA",
-#       "tran_no": "2100026",
-#       "user_id": "Nguyễn Đức Trường",
-#       "shift_id": "21194332090HKPUWKP8Z1",
-#       "store_id": "93ARMVPE4V20",
-#       "vat_sign": null,
-#       "date_last": 1603213200,
-#       "hour_last": 10,
-#       "sale_sign": null,
-#       "source_id": "10000171",
-#       "tran_date": 1603213200,
-#       "get_amount": 39600,
-#       "session_id": 0,
-#       "store_name": "NDT6896 Brand 1 Store 1 | HN",
-#       "customer_id": null,
-#       "device_code": "RMYZ8ZKV8YMZ",
-#       "minute_last": 25,
-#       "number_male": null,
-#       "print_count": 0,
-#       "vat_content": null,
-#       "vat_tran_no": null,
-#       "amount_point": 0,
-#       "coupon_count": null,
-#       "last_version": "1.12.6",
-#       "shift_charge": 0,
-#       "source_fb_id": "0",
-#       "station_code": null,
-#       "tran_no_temp": "2100026",
-#       "vat_tax_code": null,
-#       "coupon_amount": 0,
-#       "exchange_rate": 1,
-#       "membership_id": null,
-#       "number_female": null,
-#       "number_people": 1,
-#       "return_amount": 0,
-#       "store_address": {
-#         "store_latitude": 100,
-#         "store_longitude": 200
-#       },
-#       "card_info_code": null,
-#       "deposit_amount": 0,
-#       "discount_extra": 0,
-#       "origin_sale_id": "",
-#       "payment_status": null,
-#       "service_charge": 0.1,
-#       "store_latitude": 100,
-#       "tran_date_orig": 1603213200,
-#       "dinner_table_id": null,
-#       "pr_key_bookings": 0,
-#       "store_longitude": 200,
-#       "address_delivery": null,
-#       "currency_type_id": "VND",
-#       "vat_company_name": null,
-#       "membership_id_new": null,
-#       "source_voucher_id": null,
-#       "vat_customer_name": null,
-#       "membership_type_id": "",
-#       "membership_voucher": 0,
-#       "vat_payment_method": null,
-#       "membership_birthday": "",
-#       "vat_customer_address": null,
-#       "amount_discount_extra2": 0
-#     },
-#     "sale_detail": [
-#       {
-#         "fix": 0,
-#         "tax": 0,
-#         "note": "",
-#         "is_fc": 0,
-#         "amount": 36000,
-#         "is_kit": 1,
-#         "is_set": 0,
-#         "number": 4,
-#         "ots_ta": 0,
-#         "is_gift": 0,
-#         "item_id": "ITEM-MWDE",
-#         "payment": 0,
-#         "printed": 0,
-#         "sale_id": "RMYZ8ZKV8YMZ4VR6KH68J2P",
-#         "tran_id": "TA",
-#         "unit_id": "MON",
-#         "user_id": "Nguyễn Đức Trường",
-#         "discount": 0.1,
-#         "end_date": 1603213200,
-#         "hour_end": 10,
-#         "quantity": 4,
-#         "shift_id": "21194332090HKPUWKP8Z1",
-#         "price_org": 10000,
-#         "sale_date": 1603213200,
-#         "cost_price": 0,
-#         "hour_start": 10,
-#         "is_invoice": 1,
-#         "is_service": 0,
-#         "list_order": 0,
-#         "minute_end": 25,
-#         "package_id": "",
-#         "price_sale": 10000,
-#         "description": "Bánh mì 1",
-#         "is_eat_with": 0,
-#         "amount_point": 0,
-#         "minute_start": 25,
-#         "payment_type": "COD",
-#         "pr_key_order": 0,
-#         "promotion_id": "PROMOTION-V2ZN",
-#         "source_fb_id": "0",
-#         "stop_service": 0,
-#         "printed_label": 0,
-#         "is_print_label": 0,
-#         "parent_item_id": "",
-#         "promotion_name": "PROMOTION-V2ZN",
-#         "sale_detail_id": null,
-#         "temp_calculate": 0,
-#         "item_id_mapping": "",
-#         "quantity_at_temp": 0,
-#         "distribute_discount_extra2": 0
-#       }
-#     ],
-#     "sale_payment_method": [
-#       {
-#         "amount": 39600,
-#         "sale_id": "RMYZ8ZKV8YMZ4VR6KH68J2P",
-#         "user_id": "Nguyễn Đức Trường",
-#         "shift_id": "21194332090HKPUWKP8Z1",
-#         "trace_no": "RMYZ8ZKV8YMZ4VR6KH68J2P",
-#         "tran_date": 1603213200,
-#         "tran_hour": 10,
-#         "list_order": 0,
-#         "amount_orig": 39600,
-#         "tran_minute": 25,
-#         "source_fb_id": "0",
-#         "exchange_rate": 1,
-#         "amount_orig_get": 39600,
-#         "currency_type_id": "VND",
-#         "payment_method_id": "COD",
-#         "amount_orig_return": 0,
-#         "exchange_rate_return": 1,
-#         "sale_payment_method_id": null,
-#         "currency_type_id_return": "VND"
-#       }
-#     ],
-#     "is_web": false,
-#     "is_group_bill": false,
-#     "source_fb_id": "",
-#     "check_sum": "",
-#     "amount_org": 40000,
-#     "amount": 39600,
-#     "amount_discount_price": 0,
-#     "amount_discount_detail": 4000,
-#     "amount_discount_extra": 0,
-#     "amount_service_charge": 3600,
-#     "amount_vat": 0,
-#     "coupon_amount": 0,
-#     "coupon_amount_paid": 0,
-#     "ship_charge": 0,
-#     "pos_type": "POS_FABI",
-#     "store_id": "93ARMVPE4V20",
-#     "brand_id": "BRAND-8FDR",
-#     "company_id": "GRWV8ZW2XD0X"
-#   }
-# }
-
-
-
-
-    
--- application/controllers/user.py
+++ application/controllers/user.py
@@ -1,31 +1,41 @@
 from gatco.response import json, text
 from application.server import app
-from application.database import db
+from application.database import db, redisdb
 from application.extensions import auth
 from application.extensions import apimanager
+# from gatco_restapi.helpers import to_dict
 import string
 import random
+import redis 
+import uuid 
+import binascii
 
 from application.models.model import User, Role
 
-@app.route("/user_test")
-async def user_test(request):
-    return text("user_test api")
-
+def generate_user_token(uid, expire_time=86400):
+    token = binascii.hexlify(uuid.uuid4().bytes).decode()
+    session_key = "sessions:" + token
+    redisdb.set(session_key, str(uid), ex=expire_time)
+    return token
 
 @app.route("/user/login", methods=["POST", "GET"])
 async def user_login(request):
     param = request.json
     user_name = param.get("user_name")
     password = param.get("password")
+    expire_time = request.args.get("expire_time")
     print(user_name, password)
     # token = ''
     if (user_name is not None) and (password is not None):
         user = db.session.query(User).filter(User.user_name == user_name).first()
         if (user is not None) and auth.verify_password(password, user.password, user.salt):
-            token = ''.join(random.choice(string.ascii_letters) for i in range(63))
             auth.login_user(request, user)
-            return json({"id": user.id, "user_name": user.user_name,"token": token})
+            dict_user = {"id": user.id, "user_name": user.user_name}
+            dict_user["token"] = generate_user_token(dict_user['id'], expire_time)
+
+            dict_user["fullname"] = user.full_name
+
+            return json(dict_user)
         return json({"error_code":"LOGIN_FAILED","error_message":"user does not exist or incorrect password"}, status=520)
 
     else:
@@ -35,7 +45,7 @@ async def user_login(request):
 @app.route("/user/logout", methods=["GET"])
 async def user_logout(request):
     auth.logout_user(request)
-    return text("user_logout api")
+    return json({})
 
 @app.route("/user/current_user", methods=["GET"])
 async def user_current_user(request):
